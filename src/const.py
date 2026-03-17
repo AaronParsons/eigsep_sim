34e97f57b9a7ce215387b93909b130052a2f1643
Aaron Parsons <aparsons@berkeley.edu>
2026-03-17 12:59:45 -0700

    Updated const.py to import from astropy/numpy/scipy

--- src/const.py
+++ src/const.py
@@ -1,69 +1,79 @@
 """
-Module for keeping track of physical constants.  All constants in cgs units.
+Module for keeping track of physical constants.
+
+All constants are returned as plain floats in MKS (SI) units.
 See description() for a dictionary of constants and their descriptions.
 """
 
-from __future__ import print_function, division, absolute_import
+import numpy as np
+from astropy import constants as const
+from astropy import units as u
 
-pi = 3.141592653589793      # Pi
-c = 2.99792458e10           # Speed of Light
-G = 6.6726e-8               # Newton's Gravitational Constant
-h = 6.6260755e-27           # Planck constant
-e = 4.8032068e-10           # Elementary Charge
-m_e = 9.1093898e-28         # Electron Mass
-m_p = 1.6726231e-24         # Proton Mass
-k = 1.380658e-16            # Boltzmann constant
-s = 5.67051e-5              # Stefan-Boltzmann constant
-au = 1.496e13               # cm in 1 AU
-r_sun = 6.96e10             # radius of sun (cm)
-m_sun = 1.98892e33          # mass of sun (g)
-ev = 1.60217733e-12         # Electron Volts
-pc = 3.09e18                # cm in 1 pc
-km = 1e5                    # cm in 1 km
-s_per_day = 24.*60*60       # seconds in a solar day
-s_per_yr = 3.1556926e7      # seconds in a year
-sidereal_day = 86164.0908   # seconds
-len_ns = c * 1e-9           # length of a nanosecond in cm
-deg = pi / 180.             # degrees in radians
-sq_deg = 3.04617e-4         # square degree in steradians
-arcmin = deg / 60.          # arcminute in radians
-arcsec = arcmin / 60.       # arcsecond in radians
-ft = 30.48006096012         # length of a foot in cm
+# Fundamental constants (SI)
+pi = np.pi
+c = const.c.value                 # speed of light [m / s]
+G = const.G.value                 # gravitational constant [m^3 / kg / s^2]
+h = const.h.value                 # Planck constant [J s]
+e = const.e.value                 # elementary charge [C]
+m_e = const.m_e.value             # electron mass [kg]
+m_p = const.m_p.value             # proton mass [kg]
+k = const.k_B.value               # Boltzmann constant [J / K]
+sigma_sb = const.sigma_sb.value   # Stefan-Boltzmann constant [W / m^2 / K^4]
 
-# MKS constants
-Jy_mks = 1e-26              # Jansky (W / m^2 / Hz)
-R_MOON = 1_737_400          # radius of moon (m)
-R_SUN = 696_265_000         # radius of sun (m)
-R_EARTH = 6_378_137         # radius of earth (m)
+# Astronomical quantities
+au = const.au.value               # meters in 1 AU
+r_sun = const.R_sun.value         # radius of sun [m]
+m_sun = const.M_sun.value         # mass of sun [kg]
+pc = const.pc.value               # meters in 1 parsec
+
+# Time quantities
+s_per_day = u.day.to(u.s)         # seconds in a solar Earth day
+s_per_yr = u.yr.to(u.s)           # seconds in a Julian year
+sidereal_day = u.sday.to(u.s)     # seconds in a sidereal day
+
+# Derived quantities
+len_ns = c * 1e-9                 # length of a nanosecond in meters
+deg = np.pi / 180.0               # degrees in radians
+sq_deg = deg ** 2                 # square degree in steradians
+arcmin = deg / 60.0               # arcminute in radians
+arcsec = arcmin / 60.0            # arcsecond in radians
+ft = u.imperial.ft.to(u.m)        # length of a foot in meters
+
+# Radii
+R_MOON = 1_737_400.0              # radius of moon [m] (not in astropy)
+R_SUN = const.R_sun.value         # radius of sun [m]
+R_EARTH = const.R_earth.value     # radius of earth [m]
+
+# Flux units
+Jy = const.Jy.value               # Jansky [W / m^2 / Hz]
+
 
 def description():
     return {
-        'pi'          : "Pi",
-        'c'           : "Speed of Light",
-        'G'           : "Newton's Gravitational Constant",
-        'h'           : "Planck constant",
-        'e'           : "Elementary Charge",
-        'm_e'         : "Electron Mass",
-        'm_p'         : "Proton Mass",
-        'k'           : "Boltzmann constant",
-        's'           : "Stefan-Boltzmann constant",
-        'au'          : "cm in 1 AU",
-        'r_sun'       : "radius of sun (cm)",
-        'm_sun'       : "mass of sun (g)",
-        'ev'          : "Electron Volts",
-        'pc'          : "cm in 1 pc",
-        'km'          : "cm in 1 km",
-        's_per_day'   : "seconds in a solar day",
-        's_per_yr'    : "seconds in a year",
-        'sidereal_day': "seconds in a sidereal day",
-        'len_ns'      : "length of a nanosecond in cm",
-        'deg'         : "degrees in radians",
-        'sq_deg'      : "square degree in steradians",
-        'arcmin'      : "arcminute in radians",
-        'arcsec'      : "arcsecond in radians",
-        'ft'          : "length of a foot in cm",
-        'Jy_mks'      : "Jansky (W / m^2 / Hz)",
-        'R_MOON'      : "radius of moon (m)",
-        'R_SUN'       : "radius of sun (m)",
-        'R_EARTH'     : "radius of earth (m)",
+        "pi": "Pi",
+        "c": "Speed of light [m/s]",
+        "G": "Newton's gravitational constant [m^3 kg^-1 s^-2]",
+        "h": "Planck constant [J s]",
+        "e": "Elementary charge [C]",
+        "m_e": "Electron mass [kg]",
+        "m_p": "Proton mass [kg]",
+        "k": "Boltzmann constant [J/K]",
+        "sigma_sb": "Stefan-Boltzmann constant [W m^-2 K^-4]",
+        "au": "meters in 1 AU",
+        "r_sun": "radius of sun [m]",
+        "m_sun": "mass of sun [kg]",
+        "pc": "meters in 1 parsec",
+        "s_per_day": "seconds in a solar Earth day",
+        "s_per_yr": "seconds in a Julian year",
+        "sidereal_day": "seconds in a sidereal day",
+        "len_ns": "length of a nanosecond in meters",
+        "deg": "degrees in radians",
+        "sq_deg": "square degree in steradians",
+        "arcmin": "arcminute in radians",
+        "arcsec": "arcsecond in radians",
+        "ft": "length of a foot in meters",
+        "Jy": "Jansky [W / m^2 / Hz]",
+        "R_MOON": "radius of moon [m]",
+        "R_SUN": "radius of sun [m]",
+        "R_EARTH": "radius of earth [m]",
     }
